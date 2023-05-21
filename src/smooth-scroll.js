d0dac64683e8e5108969e3e5d7ba7cb89e9ef537
mnotti <markusnotti@gmail.com>
2023-05-21 13:42:27 -0700

    Fixed nav bar

--- src/smooth-scroll.js
+++ src/smooth-scroll.js
@@ -3,14 +3,14 @@ $( document ).ready( function () {
     vendorName = window.navigator.vendor,
     isOpera = window.navigator.userAgent.indexOf("OPR") > -1;
     if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false) {
-        $( "a[href*='#']" ).on( "click", function( event ) {
+       /* $( "a[href*='#']" ).on( "click", function( event ) {
             event.preventDefault();
             var href = event.target.href; 
             href = href.slice( href.indexOf( "#" ), href.length );
             $( "body" ).animate( {
                 scrollTop: $( href ).get( 0 ).offsetTop
             }, 1000 );
-    } ); 
+    } ); */ 
     } else { 
    // not Google chrome 
     }
